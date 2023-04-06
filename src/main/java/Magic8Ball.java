cc09a1cc05fa92d2566078a366123d4a26cd6a06
Tyler Lewis <tyler.lewis[6603@gmail.com>
2023-04-05 22:57:27 -0400

    8 ball done. Could just use some tweaks

--- src/main/java/Magic8Ball.java
+++ src/main/java/Magic8Ball.java
@@ -5,39 +5,88 @@ import java.util.Scanner;
  * Create a Magic 8-ball program that gives custom responses to yes or no questions
  */
 public class Magic8Ball {
+    public static void main(String[] args) {
 
-    // 1. Make a main method that includes all the steps below
+        showMenu();
+        while (true){
+            Scanner userInput = new Scanner(System.in);
+            String userQuestion = userInput.nextLine();
+            int randomNumber = new Random().nextInt(6);
 
+            if (randomNumber == 0) {
+                askMore("Forsure");
+                String answerRough = userInput.nextLine();
+                String answer = answerRough.toLowerCase().trim();
+                if (answer.equals("y")){
+                    showMenu();
+                } else if (answer.equals("n")) {
+                    break;
+                }
+            } else if (randomNumber == 1) {
+                askMore("No shot");
+                String answerRough = userInput.nextLine();
+                String answer = answerRough.toLowerCase().trim();
+                if (answer.equals("y")){
+                    showMenu();
+                } else if (answer.equals("n")) {
+                    break;
+                }
+            } else if (randomNumber == 2) {
+                askMore("80% of the time it happens 100% of the time");
+                String answerRough = userInput.nextLine();
+                String answer = answerRough.toLowerCase().trim();
+                if (answer.equals("y")){
+                    showMenu();
+                } else if (answer.equals("n")) {
+                    break;
+                }
+            } else if (randomNumber == 3) {
+                askMore("Oh yeah! That's definitely going to happen...");
+                String answerRough = userInput.nextLine();
+                String answer = answerRough.toLowerCase().trim();
+                if (answer.equals("y")){
+                    showMenu();
+                } else if (answer.equals("n")) {
+                    break;
+                }
+            } else if (randomNumber == 4) {
+                askMore("If you know, you know");
+                String answerRough = userInput.nextLine();
+                String answer = answerRough.toLowerCase().trim();
+                if (answer.equals("y")){
+                    showMenu();
+                } else if (answer.equals("n")) {
+                    break;
+                }
+            } else if (randomNumber == 5) {
+                askMore("Try again later LOL");
+                String answerRough = userInput.nextLine();
+                String answer = answerRough.toLowerCase().trim();
+                if (answer.equals("y")){
+                    showMenu();
+                } else if (answer.equals("n")) {
+                    break;
+                }
+            }
+        }
 
-    // 2. Get the user to enter a question for the magic 8-ball to answer
-    //    Scanner scanner = new Scanner(System.in);
-    //    String userQuestion = scanner.nextLine();
+        }
+        public static void showMenu() {
+            System.out.println("\n\n***************************************************");
+            System.out.println("***************************************************");
+            System.out.println("         THE MAGIC 8 BALL NOBODY ASKED FOR         ");
+            System.out.println("***************************************************");
+            System.out.println("***************************************************");
 
+            System.out.println("\n\n\n");
+            System.out.println("You may have an answer to any question you seek! Ask wisely");
+            System.out.print("What is your question? >>    ");
+    }
 
-    // 3. Make a variable and initialize it to a random number.
-    //
-    //    // Get random a number from: [0, 1, 2, 3]
-    //    int randNumber = new Random().nextInt(4);
-
-
-    // 4. If the random number is 0
-
-        // Tell the user "Yes"
-
-
-    // 5. If the random number is 1
-
-        // Tell the user "No"
-
-
-    // 6. If the random number is 2
-
-        // Tell the user a custom response
-
-
-    // 7. If the random number is 3
-
-        // Tell the user a custom response
+    public static void askMore(String message){
+        System.out.println(message + "\n\n");
+        System.out.println("Wanna ask another question??     (Y) / (N)");
+    }
 }
 
 
