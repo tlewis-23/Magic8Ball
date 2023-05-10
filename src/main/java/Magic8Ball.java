172281b3bd4719ed8560a6b06d3568798c061ab0
Tyler Lewis <tyler.lewis[6603@gmail.com>
2023-05-10 19:17:24 -0400

    Fixed 8ball

--- src/main/java/Magic8Ball.java
+++ src/main/java/Magic8Ball.java
@@ -1,9 +1,7 @@
 import java.util.Random;
 import java.util.Scanner;
 
-/*
- * Create a Magic 8-ball program that gives custom responses to yes or no questions
- */
+
 public class Magic8Ball {
     public static void main(String[] args) {
 
